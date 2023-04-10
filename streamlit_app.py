7534cc3d8be4618b9c9d57aaee09330381c005fb
mdegrauwie <122270747+mdegrauwie@users.noreply.github.com>
2023-04-10 19:10:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as components
 st.title('OpenClassRooms Project 7: Implementez un modèle de scoring')
-components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vQXaZ8TM03DGeytn0jycdfQOPzXrDsXJl5v8W_lWDNPdkJbx8mjnqv4_1fehBJub6X2CnJGtAqHDCnC/embed?start=false&loop=false&delayms=3000", width="1440", height="839", allowfullscreen="true")
+components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vQXaZ8TM03DGeytn0jycdfQOPzXrDsXJl5v8W_lWDNPdkJbx8mjnqv4_1fehBJub6X2CnJGtAqHDCnC/embed?start=false&loop=false&delayms=3000", height="839")
